4c3ffcecb4aaaad4257c4a66d828f4307a805262
MohitKalshan <mohitkalshan14.mk@gmail.com>
2023-05-12 15:39:18 +0530

    bug fixed

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      hello
       <div className="main">
         <div className="tabs">
           <div className="tab">Movies</div>
